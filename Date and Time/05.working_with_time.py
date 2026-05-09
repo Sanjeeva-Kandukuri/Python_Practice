66f2f378d72a2102154c96ee352c9bce209f4ce9
Sanjeev <ksanjeeva225@gmail.com>
2026-05-09 20:56:58 +0530

    Add working_with_time.py to demonstrate time usage

---
+++ Date and Time/05.working_with_time.py
@@ -0,0 +1,11 @@
+from datetime import time
+
+specific_time = time(14, 30, 15)
+print("Specific Time:", specific_time)
+
+
+# Extracting Hours, Minutes, and Seconds
+
+print("Hour:", specific_time.hour)
+print("Minute:", specific_time.minute)
+print("Seconds:", specific_time.second)
