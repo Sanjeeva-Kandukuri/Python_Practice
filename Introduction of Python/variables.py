801b7d1140c5cee53e83d8a910891a1af8142d3a
Sanjeev <ksanjeeva225@gmail.com>
2026-05-23 22:48:44 +0530

    Create variables.py

---
+++ Introduction of Python/variables.py
@@ -0,0 +1,30 @@
+# Variables
+
+# Definition --> variables are like containers, they store values you can change their values whenever you need.
+
+# Basic variable assignment 
+
+product_name = "loptop"
+price = 50000
+in_stock = True
+print(product_name,price,in_stock)
+
+# Multiple assignment
+
+a, b ,c = 10,20,30
+print(a,b,c)
+
+# we can also assign the same value to multiple variables
+x = y = z = 100
+print(x,y,z)
+
+# Swapping variables
+a, b = 10,20
+a,b = b,a
+print(a,b)
+
+# Deleting Variables
+
+x = 10
+del x
+print(x)
