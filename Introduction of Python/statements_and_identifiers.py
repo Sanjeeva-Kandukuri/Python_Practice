89fb75412b368ce58ea08951063d14c04f15ca9a
Sanjeev <ksanjeeva225@gmail.com>
2026-05-23 22:48:11 +0530

    Create statements_and_identifiers.py

---
+++ Introduction of Python/statements_and_identifiers.py
@@ -0,0 +1,30 @@
+# Statement
+x = 10        # --> statement 
+print(x)       # --> function call statement
+
+# Identifires
+
+age = 25                         # ---> age is an identifier
+def greet():                     # ---> greet is an identifier
+    print("Your age is ", age)
+greet()
+
+
+# Comments in python
+
+# Single line comment
+
+x = 30 # This is a single line comment
+
+# Multi-line comments [''' or """]
+
+'''this is a multiline comment
+it spans multiple lines'''
+
+print("hello, world")
+
+# Keywords in python
+
+import keyword
+print(keyword.kwlist)
+print(len(keyword.kwlist))
